40b897de643d5335d573905944d8e55c7f1be1fd
vicolau <q502338252@gmail.com>
2019-07-09 18:52:19 +0800

    resolved the bug that static file being intercepted by the interceptor

--- web/src/main/java/com/springboot/web/config/MyMvcConfig.java
+++ web/src/main/java/com/springboot/web/config/MyMvcConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -13,6 +14,11 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class MyMvcConfig implements WebMvcConfigurer {
+
+    /**
+     * 配置映射
+     * @param registry
+     */
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
         //浏览器发送 /vico 请求 也来到success页面
@@ -20,12 +26,26 @@ public class MyMvcConfig implements WebMvcConfigurer {
         registry.addViewController("/main.html").setViewName("test");
     }
 
-    //注册拦截器
+    /**
+     * 配置拦截路径
+     * @param registry
+     */
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
-        String[] excludeUrl = new String[]{"/","/index.html","/user/login","/static/**"};
+        // excludeUrl 白名单
+        String[] excludeUrl = new String[]{"/","/index.html","/user/login","/asserts/**","/webjars/**"};
         registry.addInterceptor(new LoginHandlerInterceptor()).addPathPatterns("/**")
                 .excludePathPatterns(excludeUrl);
+    }
+
+    /**
+     * 配置资源文件路径  默认是/**
+     * 也可以在配置文件中采用 spring.mvc.static-path-pattern 属性进行配置
+     * @param registry
+     */
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/static/**").addResourceLocations("classpath:/static/");
     }
 
     @Bean
